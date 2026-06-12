4cb80c8fd6d581df997564a48c3c619a6e54586f
mnonessss <belaa7696@gmail.com>
2026-06-12 13:25:07 +0300

    bridge added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import bridge from "@vkontakte/vk-bridge";
 import { AdaptivityProvider, AppRoot, ConfigProvider } from "@vkontakte/vkui";
 import "@vkontakte/vkui/dist/vkui.css";
 import App from "./App";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./styles.css";
+
+// VK Mini Apps: без VKWebAppInit клиент VK показывает «Приложение не инициализировано»
+const vkBridge = (bridge as { default?: typeof bridge }).default ?? bridge;
+if (typeof vkBridge.send === "function") {
+  void vkBridge.send("VKWebAppInit");
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
